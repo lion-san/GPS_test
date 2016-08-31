42d64f5e7f3174c3b62bea2525d85384909726a5
yokoi.shinya <shinya.yokoi@gmail.com>
2016-08-31 12:20:22 +0900

    first commit

---
+++ GPS_test.ino
@@ -0,0 +1,34 @@
+#include <SoftwareSerial.h>
+
+#define RX 21
+#define TX 20
+
+SoftwareSerial g_gps( RX, TX );
+
+void setupSoftwareSerial(){
+  g_gps.begin(9600);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  while(!Serial){
+    ;
+  }
+
+  Serial.println("GPS Start");
+
+  setupSoftwareSerial();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(g_gps.available()){
+    Serial.write(g_gps.read());
+  }
+
+  delay(100);
+
+}
